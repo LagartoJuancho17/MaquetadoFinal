88ccc0a1abc082869f9eeb1d070164d1750dcf31
Toto <tobiasarraiza17@gmail.com>
2024-11-01 21:26:30 -0300

    mis cambios locales antes del pull

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,32 @@ const observer = new IntersectionObserver((entries, observer) => {
 });
 
 observer.observe(bannerSection);
+
+
+/*NAVBAR DESAPARECEE!!! */
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > lastScrollY) {
+        // Si el usuario hace scroll hacia abajo, oculta el navbar
+        navbar.classList.add("hidden");
+    } else {
+        // Si el usuario hace scroll hacia arriba, muestra el navbar y cambia su color a negro
+        navbar.classList.remove("hidden");
+        navbar.classList.add("black");
+    }
+
+    // Actualiza la posición del último scroll
+    lastScrollY = window.scrollY;
+
+    // Si está en la parte superior de la página, vuelve al color original
+    if (window.scrollY == 0) {
+        navbar.classList.remove("black");
+    }
+
+});
+
+
